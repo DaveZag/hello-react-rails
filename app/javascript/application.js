2baaa9bdcb69b51f85096e38717ebcc4a5595b51
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-02-28 20:51:46 +0200

    Add react code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,4 +10,10 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
